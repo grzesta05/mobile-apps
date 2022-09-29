f87b1a90fce85b85ac9b4683f9d42837db8c6305
Marcin Majgier <marcin0920@gmail.com>
2022-09-29 22:06:57 +0200

    finish Game cycle handling

--- nobody console game/nobody/main.swift
+++ nobody console game/nobody/main.swift
@@ -12,7 +12,7 @@ class Game : ParagraphGameReq{
     var outbreak_risk = 0.01
     var currentEventID = "0"
     var nextEvents : [String] = []
-    var changes = [0,0,0,0]
+    var changes : [Double] = [0,0,0,0]
     
     init() {
         while(losingCondition())
@@ -21,34 +21,41 @@ class Game : ParagraphGameReq{
             optionsPrompt()
             getRandomEvent()
             currentEventID = nextEvents.remove(at: 0)
+            print("", terminator: Array(repeating: "\n", count: 100).joined())
+            
         }
+        print("Would you like to play again?", terminator: "\n")
         
     }
     
     func applyConsequences(consequence : Consequence)
     {
         resources += consequence.change_resources
+        changes[0] = consequence.change_resources
         safety += consequence.change_safety
+        changes[1] = consequence.change_safety
         social_trust += consequence.change_social_trust
+        changes[2] = consequence.change_social_trust
         outbreak_risk += consequence.change_outbreak_risk
-        
+        changes[3] = consequence.change_outbreak_risk
     }
     
     func optionsPrompt() {
         var options : [Option] = main_events[currentEventID]!.options
         for (i, a) in options.enumerated(){
-            print("\(a.content) - press \(i)")
+            print("\(a.content) - press \(i)", terminator: "\n")
         }
         let chosen: Int = Int(readLine()!) ?? 0
+        applyConsequences(consequence: options[chosen].result)
     }
     func getRandomEvent() -> Void
     {
-        let randomNumber = Int.random(in: 0..<main_events.count)
+        let randomNumber = Int.random(in: 1..<main_events.count)
         nextEvents.append(String(randomNumber))
     }
     
     func losingCondition() -> Bool {
-        var fail : Fail? = nil
+        var fail: Fail?
         if(resources <= 0){
             fail = Fail(type: Factors.Resources)
         }
@@ -64,7 +71,7 @@ class Game : ParagraphGameReq{
         {
             fail = Fail(type: Factors.OutbreakRisk)
         }
-        if(fail! == nil)
+        if(!(fail == nil))
         {
             fail!.printFailMessage(stats: [resources, safety, social_trust, outbreak_risk]);
             return false;
@@ -75,13 +82,15 @@ class Game : ParagraphGameReq{
     func setEvent() -> Void {}
     func printState()  -> Void{
         let output : String = String(format: """
-    Resources:     %d \(changes[0] > 0 ? "+ " : "- ") \(changes[0])
-    Safety:        %d \(changes[1] > 0 ? "+ " : "- ") \(changes[1])
-    Social Trust:  %d \(changes[2] > 0 ? "+ " : "- ") \(changes[2])
-    Outbreak Risk: %f% \(changes[3] > 0 ? "+ " : "- ") \(changes[3])
-
+    Resources:     %f \(changes[0] > 0 ? "+ " : "- ") \(changes[0])
+    Safety:        %f \(changes[1] > 0 ? "+ " : "- ") \(changes[1])
+    Social Trust:  %f \(changes[2] > 0 ? "+ " : "- ") \(changes[2])
+    Outbreak Risk: %f %% \(changes[3] > 0 ? "+ " : "- ") \(changes[3])
+    \n
+    \n
     %@
     %@
+    \n
 """, resources, safety, social_trust, outbreak_risk, main_events[currentEventID]!.title, main_events[currentEventID]!.description)
         print(output)
     }
--- nobody console game/nobody/mainEvents.swift
+++ nobody console game/nobody/mainEvents.swift
@@ -10,8 +10,8 @@ import Foundation
 var main_events = [
     "0" : Event(
         title : "Mors ad Virum",
-        description: "So it begins",
-        options : []
+        description: "Tough times came. We… We didn’t realise how important the virus development would be… At first, we created it to create superhumans, to make us stronger, more self-reliant and immune. Something went terribly wrong… You are nobody. Chosen by the people to lead them through the tough times. Choose wisely",
+        options : [Option(content: "Alright...", result: Consequence(0,0,0,0))]
     ),
 //    "resourcesFail" : Fail(),
 //    "outbreak_riskFail": Fail(),
